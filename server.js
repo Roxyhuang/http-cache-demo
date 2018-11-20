aef06e75442d21401ae4f02e5af5677852730785
roxy_huang <sixin_huang@outlook.com>
2018-11-20 15:40:05 +0800

    update demo

--- server.js
+++ server.js
@@ -3,16 +3,26 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   // 主页面
   if (req.url === '/' || req.url === '' || req.url === '/index.html') {
-    fs.readFile('./assets/index.html', function (err, file) {
-      console.log(req.url);
-      //对主文档设置缓存，无效果
-      // res.setHeader('Cache-Control', "no-cache"); // 不缓存主文档
-      res.setHeader('Cache-Control', "max-age=" + 2592071); //缓存五分钟
-      // res.setHeader('Expires', new Date(Date.now() + 5000000)); //缓存五分钟
-      res.setHeader('Content-Type', 'text/html');
-      res.writeHead('200', "OK");
-      res.end(file);
-    });
+    const tag = req.headers['if-none-match'];
+    if(tag === '33a64df551425fcc55e4d42a148795d9f25f89d9') {
+      res.writeHead('304', 'Not Modified');
+      res.end();
+    } else {
+      fs.readFile('./assets/index.html', function (err, file) {
+        console.log(req.url);
+        //对主文档设置缓存，无效果
+        res.setHeader('Cache-Control', "max-age=" + 300); //缓存五分钟
+        res.setHeader('Expires', new Date(Date.now() + 300000)); //缓存五分钟
+
+        // 默认设置了五分钟缓存，可以注销以下注释并注册上面两行代码，测试不缓存主文档
+        // res.setHeader('Cache-Control', "no-cache"); // 不缓存主文档
+        
+        res.setHeader('ETag', '33a64df551425fcc55e4d42a148795d9f25f89d9');
+        res.setHeader('Content-Type', 'text/html');
+        res.writeHead('200', "OK");
+        res.end(file);
+      });
+    }
   }
   // 跳转页面
   if (req.url === '/temp.html') {
@@ -57,6 +67,24 @@ http.createServer(function (req, res) {
         res.setHeader('ETag', '33a64df551425fcc55e4d42a148795d9f25f89d4');
         res.setHeader('Last-Modified', 'Fri Nov 16 2018 08:32:02 GMT+0800 (CST)');
         res.setHeader('Content-Type', 'image/png');
+        res.writeHead('200', 'OK');
+        res.end(file);
+      });
+    }
+  }
+
+  if (req.url === '/favicon.ico') {
+    const tag = req.headers['if-none-match'];
+    if(tag === '33a64df551425fcc55e4d42a148795d9f25f89d4') {
+      res.writeHead('304', 'Not Modified');
+      res.end();
+    } else {
+      fs.readFile('./assets/favicon.ico', function (err, file) {
+        res.setHeader('Cache-Control', "max-age=" + 300); //缓存五分钟
+        res.setHeader('Expires', new Date(Date.now() + 300000)); //缓存五分钟
+        res.setHeader('ETag', '33a64df551425fcc55e4d42a148795d9f25f89d4');
+        res.setHeader('Last-Modified', 'Fri Nov 16 2018 08:32:02 GMT+0800 (CST)');
+        res.setHeader('Content-Type', 'image/x-icon');
         res.writeHead('200', 'OK');
         res.end(file);
       });
